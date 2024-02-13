a3bbb86346a74167d594149a7bdbb4664187cc0f
AshFungor <evgenynarkhov2@gmail.com>
2024-02-13 23:00:59 +0300

    adding cypher test

--- src/main.cpp
+++ src/main.cpp
@@ -85,26 +85,53 @@ int main() {
     // }
 
     // 8e 73 b0 f7 da 0e 64 52 c8 10 f3 2b 80 90 79 e5 62 f8 ea d2 52 2c 6b 7b
-    auto key = AES::Key<192>{
-        .mode = AES::Key<192>::Mode::AES_192,
+    // auto key = AES::Key<192>{
+    //     .mode = AES::Key<192>::Mode::AES_192,
+    //     .raw = {
+    //         0x8e, 0x73, 0xb0, 0xf7,
+    //         0xda, 0x0e, 0x64, 0x52,
+    //         0xc8, 0x10, 0xf3, 0x2b,
+    //         0x80, 0x90, 0x79, 0xe5,
+    //         0x62, 0xf8, 0xea, 0xd2,
+    //         0x52, 0x2c, 0x6b, 0x7b}
+    // };
+    // std::cout << std::hex;
+    // auto routine = AES::key_extension(key);
+    // int i = 0;
+    // for (auto& w : routine) {
+    //     std::cout << "word" << i << ": ";
+    //     for (auto& b : w) {
+    //         std::cout << (int) b;
+    //     }
+    //     std::cout << '\n';
+    //     ++i;
+    // }
+
+    // 54 68 61 74 73 20 6D 79 20 4B 75 6E 67 20 46 75
+    auto key = AES::Key<128>{
+        .mode = AES::Key<128>::Mode::AES_128,
         .raw = {
-            0x8e, 0x73, 0xb0, 0xf7,
-            0xda, 0x0e, 0x64, 0x52,
-            0xc8, 0x10, 0xf3, 0x2b,
-            0x80, 0x90, 0x79, 0xe5,
-            0x62, 0xf8, 0xea, 0xd2,
-            0x52, 0x2c, 0x6b, 0x7b}
+            0x54, 0x68, 0x61, 0x74,
+            0x73, 0x20, 0x6d, 0x79,
+            0x20, 0x4b, 0x75, 0x6e,
+            0x67, 0x20, 0x46, 0x75}
     };
-    std::cout << std::hex;
-    auto routine = AES::key_extension(key);
-    int i = 0;
-    for (auto& w : routine) {
-        std::cout << "word" << i << ": ";
-        for (auto& b : w) {
-            std::cout << (int) b;
+    // 54 77 6F 20 4F 6E 65 20 4E 69 6E 65 20 54 77 6F
+    AES::Block in {
+        {{0x54, 0x77, 0x6f, 0x20},
+        {0x4f, 0x6e, 0x65, 0x20},
+        {0x4e, 0x69, 0x6e, 0x65},
+        {0x20, 0x54, 0x77, 0x6f}}
+    };
+
+    auto result = AES::cipher(in, key);
+
+    std::cout << "Expected: 29 C3 50 5F 57 14 20 F6 40 22 99 B3 1A 02 D7 3A\n";
+    std::cout << "Actual: " << std::hex;
+    for (auto& row: result) {
+        for (auto& el : row) {
+            std::cout << (int) el << ' ';
         }
-        std::cout << '\n';
-        ++i;
     }
 
     return 0;
